e8b923f54aff8c63d62adcbad66f9394f39e93ea
Naveen Kumar <naveen@spliceglobal.com>
2017-09-13 11:51:25 +0530

    Intial commit

---
+++ app/src/main/java/com/project/spliceglobal/recallgo/RegisterActivity.java
@@ -0,0 +1,40 @@
+package com.project.spliceglobal.recallgo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RegisterActivity extends AppCompatActivity {
+    CircleImageView profile_image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar actionBar=getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("SignUp");
+        }
+        profile_image = (CircleImageView) findViewById(R.id.profile_image);
+        profile_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(i.createChooser(i, "select pic"), 101);
+            }
+        });
+
+
+
+    }
+}
